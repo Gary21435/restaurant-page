e17bcff869f53e18064e886e18cf204949d5d3dc
gegi <35646675+Gary21435@users.noreply.github.com>
2025-05-15 13:01:33 -0700

    Add scripts to package.json and delete modules for template usage

--- src/about.js
+++
@@ -1,9 +0,0 @@
-export function aboutPage() {
-    const about = document.createElement("div");
-    about.className = "page";
-
-    const p = document.createElement("h1");
-    about.appendChild(p);
-    document.querySelector("#content").appendChild(about);
-    p.textContent = "Established in 1986.";
-}
--- src/menu.js
+++
@@ -1,9 +0,0 @@
-export function menuPage() {
-    const menu = document.createElement("div");
-    menu.className = "page";
-
-    const p = document.createElement("h1");
-    menu.appendChild(p);
-    document.querySelector("#content").appendChild(menu);
-    p.textContent = "This is a menu"
-}
